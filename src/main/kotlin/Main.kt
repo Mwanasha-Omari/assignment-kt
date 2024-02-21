78f0381a9ef098a450cb4978bbb38e81d5be3b07
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-02-21 00:18:38 -0500

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+var y =result(23,31,5,4)
+println()
+
+val name ="Hello Omari"
+    println(name)
+
+var num1=9
+var num2=2
+var remainder=num1%num2
+println(remainder)
+
+    val string ="I like playing table tennis"
+    val sentence ="$string"
+    println(sentence)
+    //output
+
+
+}
+fun result(num1:Int,num2:Int,num3:Int ,num4:Int):{
+    var result=num1+num2+num3+num4
+    return result
+}
+
+
+
